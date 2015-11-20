3c1568cba9933b2766044d4e4bfac67d9bce4637
Eduardo Vital <vitaldu@gmail.com>
2015-11-20 11:40:13 -0200

    Fixed javascript turbolink bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 //= require materialize-sprockets
 //= require materialize/extras/nouislider
+//= require turbolinks
 
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
