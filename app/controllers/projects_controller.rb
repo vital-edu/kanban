da75d4fd8dd41f0305b0f9c80cdd17143819d594
Eduardo Vital <vitaldu@gmail.com>
2015-11-20 18:11:06 -0200

    New approach to edition and creating

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -45,7 +45,7 @@ class ProjectsController < ApplicationController
   def update
     respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.html { redirect_to projects_url, notice: 'Project was successfully updated.' }
         format.json { render :show, status: :ok, location: @project }
       else
         format.html { render :edit }
