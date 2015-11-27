34a03cd5c42ef3669e89dae26516820ddf6a496c
Eduardo Vital <vitaldu@gmail.com>
2015-11-27 13:07:25 -0200

    Fixed priority of tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,21 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :stage
   validates :name, :priority, :description, presence: true
-  validates :priority, numericality: { only_integer: true }
+  validate :validate_priority
 
   after_save :update_project_progress
 
+  def priority_list
+    ['Baixa', 'Média', 'Alta']
+  end
+
 private
+  def validate_priority
+    if priority_list.index(self.priority).nil?
+      errors.add("Prioridade", " inválida")
+    end
+  end
+
   def update_project_progress
     Project.find(self.project.id).update_progress
   end
